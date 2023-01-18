646f0454a61293d9562671516c56bcbc6612a101
Tavleen Kaur <122363068+Tavleen1203@users.noreply.github.com>
2023-01-18 23:11:37 +0530

    Create A1_Q1.java
    Submitting Question1 of Assignment 1

---
+++ A1_Q1.java
@@ -0,0 +1,13 @@
+
+
+public class A1_Q1 {
+
+  public static void main(String[] args) {
+    System.out.print("*****\n");
+    System.out.print("   * \n");
+    System.out.print("  *  \n");
+    System.out.print(" *   \n");
+    System.out.print("*****");
+
+  }
+}
