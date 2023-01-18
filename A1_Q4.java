6001cc01222fd215947ea254e489c1e4129a1686
Tavleen Kaur <122363068+Tavleen1203@users.noreply.github.com>
2023-01-19 00:06:58 +0530

    Create A1_Q4.java
    Submitting Question 4 of Assignment1

---
+++ A1_Q4.java
@@ -0,0 +1,25 @@
+public class A1_Q4 {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int low=scn.nextInt();
+        int high=scn.nextInt();
+        
+        for(int n=low; n<=high; n++){
+            int count=0;
+            
+            for(int div=2; div*div<=n; div++){
+                if(n%div==0){
+                    count++;
+                    break;
+                }
+            }
+            
+            
+            if(count==0){
+            System.out.println(n);
+            
+        }
+    }
+} 
+    
+}
