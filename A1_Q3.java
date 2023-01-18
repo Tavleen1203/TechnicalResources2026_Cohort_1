6a031847a08e00b5ebda6c5588294219a270444d
Tavleen Kaur <122363068+Tavleen1203@users.noreply.github.com>
2023-01-18 23:46:42 +0530

    Create A1_Q3.java
    Submitting Question 3 of Assignment 1

---
+++ A1_Q3.java
@@ -0,0 +1,24 @@
+public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int a=scn.nextInt();
+      
+      for(int1=0; i<a; i++){
+          int b=scn.nextInt();
+          
+          for(int div=1; div<=n; div++){
+              if(n%div==0){
+                  count++;
+              }
+          }
+  
+       if(count==2){
+           System.out.println("Prime Number");
+        
+       }else{
+           System.out.println("Not a Prime Number");
+       }
+  
+   }
+  }
