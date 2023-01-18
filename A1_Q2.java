79dbc8a49694ded1ba25293636b4baa93b280950
Tavleen Kaur <122363068+Tavleen1203@users.noreply.github.com>
2023-01-18 23:27:26 +0530

    Create A1_Q2.java
    Submitting Question 2 of Assignment 1

---
+++ A1_Q2.java
@@ -0,0 +1,30 @@
+public class A1_Q2 {
+
+    public static void main(String[] args) {
+      // input - don't change this code
+      Scanner scn = new Scanner(System.in);
+      int marks = scn.nextInt();
+      // input - don't change this code
+  
+      // code here
+      if(marks>90){
+          System.out.println("Excellent");
+      
+      }else if(marks>80){
+          System.out.println("Good");
+  
+      }else if(marks>70){
+          System.out.println("Fair");
+          
+      }else if(marks>60){
+          System.out.println("Meets Expectations");
+          
+      }else{
+          System.out.println("Below Par");
+          
+      }
+    }
+ 
+    
+  }
+  
